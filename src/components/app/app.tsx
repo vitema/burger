5c2ab0efd1d5c3a594718c8845e4336b428a62bc
vitema <vitema@list.ru>
2022-12-17 13:51:00 +0300

    configure for deploy

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -129,7 +129,7 @@ function App() {
   };
   return (
     <Provider store={store}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <ModalSwitch />
       </Router>
     </Provider>
